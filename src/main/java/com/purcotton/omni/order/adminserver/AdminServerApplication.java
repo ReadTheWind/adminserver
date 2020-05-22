1e03efbd0d7e6c74ef26645df43e0c3632dc5533
liuhuan <31094897+ReadTheWind@users.noreply.github.com>
2020-05-22 16:52:44 +0800

    spring boot admin 整合 spring security  提交

---
+++ src/main/java/com/purcotton/omni/order/adminserver/AdminServerApplication.java
@@ -0,0 +1,22 @@
+package com.purcotton.omni.order.adminserver;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purcotton/omni/order/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purcotton.omni.order.adminserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
